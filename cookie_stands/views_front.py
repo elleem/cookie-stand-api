7a6bb9846da5685f205a674d6bcbceb9144416cf
Lauren <lauren.main28@gmail.com>
2023-07-10 22:00:48 -0700

    updates readme

--- cookie_stands/views_front.py
+++ cookie_stands/views_front.py
@@ -5,29 +5,29 @@ from .models import CookieStand
 
 
 class CookieStandListView(LoginRequiredMixin, ListView):
-    template_name = "things/cookiestand_list.html"
+    template_name = "stands/cookiestand_list.html"
     model = CookieStand
     context_object_name = "cookie_stands"
 
 
 class CookieStandDetailView(LoginRequiredMixin, DetailView):
-    template_name = "things/cookiestand_detail.html"
+    template_name = "stands/cookiestand_detail.html"
     model = CookieStand
     # context_object_name = "cookie_stands"
 
 class CookieStandUpdateView(LoginRequiredMixin, UpdateView):
-    template_name = "things/cookiestand_update.html"
+    template_name = "stands/cookiestand_update.html"
     model = CookieStand #cookiestand
     fields = "__all__"
     success_url = reverse_lazy("cookiestand_list")
 
 class CookieStandCreateView(LoginRequiredMixin, CreateView):
-    template_name = "things/cookiestand_create.html"
+    template_name = "stands/cookiestand_create.html"
     model = CookieStand
     fields =  "__all__"
     success_url = reverse_lazy("cookiestand_list")
 
 class CookieStandDeleteView(LoginRequiredMixin, DeleteView):
-    template_name = "things/cookiestand_delete.html"
+    template_name = "stands/cookiestand_delete.html"
     model = CookieStand
     success_url = reverse_lazy("cookiestand_list")
